3ff69f83c1b5ff4e03931ce396896600e8ea9e36
DevExpressExampleBot <fakeEmail>
2024-11-21 18:56:55 +0400

    Source auto update [skip ci]

--- VB/Retain_Img_Src/Form1.Designer.vb
+++ VB/Retain_Img_Src/Form1.Designer.vb
@@ -1,83 +1,82 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace Retain_Img_Src
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
-			Me.memoEdit1 = New DevExpress.XtraEditors.MemoEdit()
-			Me.splitterControl1 = New DevExpress.XtraEditors.SplitterControl()
-			CType(Me.memoEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' richEditControl1
-			' 
-			Me.richEditControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.richEditControl1.Location = New System.Drawing.Point(0, 0)
-			Me.richEditControl1.Name = "richEditControl1"
-			Me.richEditControl1.Size = New System.Drawing.Size(533, 453)
-			Me.richEditControl1.TabIndex = 0
-			Me.richEditControl1.Text = "richEditControl1"
-'			Me.richEditControl1.DocumentLoaded += New System.EventHandler(Me.richEditControl1_DocumentLoaded);
-			' 
-			' memoEdit1
-			' 
-			Me.memoEdit1.Dock = System.Windows.Forms.DockStyle.Right
-			Me.memoEdit1.Location = New System.Drawing.Point(538, 0)
-			Me.memoEdit1.Name = "memoEdit1"
-			Me.memoEdit1.Size = New System.Drawing.Size(250, 453)
-			Me.memoEdit1.TabIndex = 1
-			' 
-			' splitterControl1
-			' 
-			Me.splitterControl1.Dock = System.Windows.Forms.DockStyle.Right
-			Me.splitterControl1.Location = New System.Drawing.Point(533, 0)
-			Me.splitterControl1.Name = "splitterControl1"
-			Me.splitterControl1.Size = New System.Drawing.Size(5, 453)
-			Me.splitterControl1.TabIndex = 2
-			Me.splitterControl1.TabStop = False
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(788, 453)
-			Me.Controls.Add(Me.richEditControl1)
-			Me.Controls.Add(Me.splitterControl1)
-			Me.Controls.Add(Me.memoEdit1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.memoEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
+            Me.memoEdit1 = New DevExpress.XtraEditors.MemoEdit()
+            Me.splitterControl1 = New DevExpress.XtraEditors.SplitterControl()
+            CType((Me.memoEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' richEditControl1
+            ' 
+            Me.richEditControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.richEditControl1.Location = New System.Drawing.Point(0, 0)
+            Me.richEditControl1.Name = "richEditControl1"
+            Me.richEditControl1.Size = New System.Drawing.Size(533, 453)
+            Me.richEditControl1.TabIndex = 0
+            Me.richEditControl1.Text = "richEditControl1"
+            AddHandler Me.richEditControl1.DocumentLoaded, New System.EventHandler(AddressOf Me.richEditControl1_DocumentLoaded)
+            ' 
+            ' memoEdit1
+            ' 
+            Me.memoEdit1.Dock = System.Windows.Forms.DockStyle.Right
+            Me.memoEdit1.Location = New System.Drawing.Point(538, 0)
+            Me.memoEdit1.Name = "memoEdit1"
+            Me.memoEdit1.Size = New System.Drawing.Size(250, 453)
+            Me.memoEdit1.TabIndex = 1
+            ' 
+            ' splitterControl1
+            ' 
+            Me.splitterControl1.Dock = System.Windows.Forms.DockStyle.Right
+            Me.splitterControl1.Location = New System.Drawing.Point(533, 0)
+            Me.splitterControl1.Name = "splitterControl1"
+            Me.splitterControl1.Size = New System.Drawing.Size(5, 453)
+            Me.splitterControl1.TabIndex = 2
+            Me.splitterControl1.TabStop = False
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(788, 453)
+            Me.Controls.Add(Me.richEditControl1)
+            Me.Controls.Add(Me.splitterControl1)
+            Me.Controls.Add(Me.memoEdit1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.memoEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
-		Private memoEdit1 As DevExpress.XtraEditors.MemoEdit
-		Private splitterControl1 As DevExpress.XtraEditors.SplitterControl
-	End Class
+'#End Region
+        Private richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
+
+        Private memoEdit1 As DevExpress.XtraEditors.MemoEdit
+
+        Private splitterControl1 As DevExpress.XtraEditors.SplitterControl
+    End Class
 End Namespace
-
